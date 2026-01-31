f670810d3d3243af8a26a4662ef6c25042bf928a
Laszlo Magyar <lmagyar1973@gmail.com>
2026-01-31 22:59:00 +0100

    fix jq - addons, repositories

--- lib/repositories.sh
+++ lib/repositories.sh
@@ -72,6 +72,10 @@ function bashio::repositories() {
     response="${info}"
     if ! bashio::var.false "${filter}"; then
         response=$(bashio::jq "${info}" "${filter}")
+        if [ "$?" -ne "${__BASHIO_EXIT_OK}" ]; then
+            bashio::log.error "Failed to execute the jq filter"
+            return "${__BASHIO_EXIT_NOK}"
+        fi
         if ! bashio::var.false "${cache_key}"; then
             bashio::cache.set "${cache_key}" "${response}"
         fi
